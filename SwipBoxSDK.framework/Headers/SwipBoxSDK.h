233dfc61ad5990e6193648f9032ce14cea8faf92
Spyros Spatharis <s.spatharis@skroutz.gr>
2022-02-07 15:21:48 +0200

    Added files to git lfs

--- SwipBoxSDK.framework/Headers/SwipBoxSDK.h
+++ SwipBoxSDK.framework/Headers/SwipBoxSDK.h
@@ -1,19 +1,3 @@
-//
-//  SwipBoxSDK.h
-//  SwipBoxSDK
-//
-//  Created by Muzammil on 26/10/2020.
-//  Copyright © 2020 Swipbox. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for SwipBoxSDK.
-FOUNDATION_EXPORT double SwipBoxSDKVersionNumber;
-
-//! Project version string for SwipBoxSDK.
-FOUNDATION_EXPORT const unsigned char SwipBoxSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwipBoxSDK/PublicHeader.h>
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:46207db33788f8e58e602c0b2ac81a6ac94ba8e7b248d2118a9b951dcfa4b7e0
+size 509
