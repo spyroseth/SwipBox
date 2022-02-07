364edb0b168bb964b7f95645a164070437fe37ba
Spyros Spatharis <spyrospath@gmail.com>
2022-02-07 15:25:36 +0200

    Add Swiftbox repo

---
+++ Swiftbox SDK/SwipBoxSDK.framework/Headers/SwipBoxSDK.h
@@ -0,0 +1,19 @@
+//
+//  SwipBoxSDK.h
+//  SwipBoxSDK
+//
+//  Created by Muzammil on 26/10/2020.
+//  Copyright © 2020 Swipbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwipBoxSDK.
+FOUNDATION_EXPORT double SwipBoxSDKVersionNumber;
+
+//! Project version string for SwipBoxSDK.
+FOUNDATION_EXPORT const unsigned char SwipBoxSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipBoxSDK/PublicHeader.h>
+
+
